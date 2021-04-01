e40a69796be0a22e2f34ca3bcb5e38fb79abc5d3
VictorCuevas98 <50884654+VictorCuevas98@users.noreply.github.com>
2021-03-31 19:22:54 -0600

    other commit

--- assets/js/portafolio.js
+++ assets/js/portafolio.js
@@ -3,28 +3,28 @@ var portfolioData = [{
     'subtitle': 'Gestion en tiempo real de mermas para Jugos del Valle',
     'description': 'MYC (Monitoreo y Control de Merma) fue desarrollado y diseñado para gestionar las mermas en tiempo real usando Laravel, OAuth2, JQuery, JavaScript, Mysql, creando API Rest y consumiendo los servicios desde un Cliente HTTP, es un sistema web diseñado por un equipo de desarrolladores de la <a href="https://www.ubam.com.mx/comunidaduniversitaria.html">Universidad Bancaria de México</a> para Coordinadores de <a href="https://jugosdelvalle.com.mx/">Jugos del Valle</a>.',
     'code': 'monitoreo',
-    'buttons': ['desktop', false, false, 'http://monitoreoControl.com'],
+    'buttons': ['assets/video/videomonitoreo.pm4', 'http://monitoreoControl.com'],
     'asset': 'assets/img/portafolio/monitorecontrol.jpg'
 }, {
     'title': 'Xynergia Club Contenido',
     'subtitle': 'Promo Landing Page HotGirls para Perú',
     'description': 'Pagina web que fue diseñada atractivamente con la ayuda de Sketch, Adobe XD y programada con Bootrsap, CSS3 (sass), JavaSCript ES6, es capaz de mostrar contenido dinamico con una sola seccion adminitrativa de contenido. (CMS diseñado por desarrolladores externos) ',
     'code': 'club',
-    'buttons': ['desktop', false, false, 'http://xynlc.com/hotsex/af_xyn_pe_entel_sexy_001/?XID=958&ani='],
+    'buttons': [false, 'http://xynlc.com/hotsex/af_xyn_pe_entel_sexy_001/?XID=958&ani='],
     'asset': 'assets/img/portafolio/clubcontenido.jpg'
 }, {
     'title': 'Xynergia Servicios Avanzados',
     'subtitle': 'Web de Contacto Responsivo',
     'description': 'Xynergia.com es una pagina web diseñada de forma responsiva por desarrolladores de <a href="www.xynergia.com">Xynergia</a> usando CSS3, Bootrstrap, JavaScript, JQuery, Illustrator, y patrones de diseño. (Atomic Design)',
     'code': 'xynergia',
-    'buttons': ['desktop', false, false, 'http://xynergia.com'],
+    'buttons': [false, 'http://xynergia.com'],
     'asset': 'assets/img/portafolio/xynergia.jpg'
 },{
     'title': 'Xynergia HotGirls',
     'subtitle': 'Promo Landing Page HotGirls para Perú',
     'description': 'HotGirls es el dominio de una landing page diseñada con la ayuda de CSS3, Bootrstrap, es capaz de mostrar contenido dinamico con una sola seccion adminitrativa de contenido. (CMS diseñado por desarrolladores externos)',
     'code': 'hotgirls',
-    'buttons': ['desktop', false, false, 'http://xynlc.com/latinbabes/af_pe_entel_sexy_001/?XID=957&ani='],
+    'buttons': [false, 'http://xynlc.com/latinbabes/af_pe_entel_sexy_001/?XID=957&ani='],
     'asset': 'assets/img/portafolio/hotgirl.jpg'
 }];
 
@@ -35,7 +35,8 @@ var currentProjectIndex = 0;
 
 var $portfolioModal = $('#portafolio-modal'),
     $portfolioBtn = $('.btn-portafolio'),
-    $imgmodal = $('#img-modal');
+    $imgmodal = $('#img-modal'),
+    $btnlink = $('#btn-link');
 
 //portfolio click
 $portfolioBtn.click(function () {
@@ -84,17 +85,18 @@ function changeProject(index) {
     $subtitle.text(portfolioData[index].subtitle);
     $description.html(portfolioData[index].description);
     $imgmodal.attr('src', portfolioData[index].asset);
+    $btnlink.attr('href', portfolioData[index].buttons[1]);
     //updates buttons
-   /*
+   
     var buttons = portfolioData[index].buttons;
     for (var i = 0; i < buttons.length; i++) {
         if (buttons[i] === false) {
-            $($devices[i]).addClass('hide');
+            $($devices[i]).addClass('visually-hidden');
         } else {
-            $($devices[i]).removeClass('hide');
+            $($devices[i]).removeClass('visually-hidden');
         }
     }
-   */
+
     //resets video playback (stops) and changes poster
     //resetVideo(index);
 }
